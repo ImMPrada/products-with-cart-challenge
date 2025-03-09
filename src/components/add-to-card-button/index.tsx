2479d2b49b69c2c60554fd375503e326fa0dd650
immprada <im.mprada@gmail.com>
2025-03-08 22:27:52 -0500

    refactor: reorganize assets and update dependencies - Move SVG assets from public to src directory, update dependencies, add TypeScript SVG types

--- src/components/add-to-card-button/index.tsx
+++ src/components/add-to-card-button/index.tsx
@@ -1,5 +1,7 @@
 import { type AddToCartButtonProps } from './types';
 import './styles.scss';
+import AddToCartIcon from '../../assets/icon-add-to-cart.svg?react';
+
 
 export const AddToCartButton = ({
   title,
@@ -8,6 +10,7 @@ export const AddToCartButton = ({
   return (
     <div className="add-to-cart-button">
       <button className="add-to-cart-button__button text--bold">
+        <AddToCartIcon />
         {title}
       </button>
     </div>
