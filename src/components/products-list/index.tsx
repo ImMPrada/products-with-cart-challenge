96de3c63ff0e62cf55d89c2c402ba382728af1a7
immprada <im.mprada@gmail.com>
2025-03-09 00:10:16 -0500

    feat: implement responsive grid layout for products list with specific spacing

--- src/components/products-list/index.tsx
+++ src/components/products-list/index.tsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
 import { ProductsContext } from '../../contexts/products-context';
 import { ProductCard } from '../product-card';
+import './styles.scss';
 
 function ProductsList() {
   const { products } = useContext(ProductsContext);
@@ -10,11 +11,13 @@ function ProductsList() {
   }, [products]);
 
   return (
-    <div className="">
-      <h1>Products</h1>
-      {products.map((product) => (
-        <ProductCard key={product.uuid} product={product} />
-      ))}
+    <div className="products-list">
+      <h1 className="products-list__title text--bold">Desserts</h1>
+      <div className="products-list__container">
+        {products.map((product) => (
+          <ProductCard key={product.uuid} product={product} />
+        ))}
+      </div>
     </div>
   )
 }
